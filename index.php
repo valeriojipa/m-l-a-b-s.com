d3907b9dac837e213e20d4ec1308b4a0378c3765
valeriojipa <33082242+valeriojipa@users.noreply.github.com>
2017-10-25 02:38:26 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,11 @@
-<!--
-Desenvolvido Por: mLabs
--->
-<?php
-$url = "http://m-l-a-b-s.com/";
-$title = "mLabs";
-
-$var = array(
-        "1" => array(
-                  "titulo" => "mLabs",
-                  "image" => "http://gph.is/2m6xCjt"
-                  ),
-        "2" => array(
-                  "titulo" => "mLabs",
-                  "image" => "http://gph.is/1pptU1s"
-                  ),
-        "3" => array(
-                  "titulo" => "mLabs",
-                  "image" => "http://gph.is/NYwUyC"
-                  ),
-        );
-
-$section = array_rand($var);
-
-?>
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-<meta http-equiv="content-type" content="text/html; charset=utf-8">
-<meta property="og:locale" content="pt_BR">
-<meta property="og:title" content="<?php echo $var[$section]['titulo']; ?>" />
-<meta property="og:url" content="<?php echo $var[$section]['image']; ?>" />
-<meta property="og:site_name" content="" />
-<meta property="og:description" content="" />
-<meta property="og:image" content="<?php echo $var[$section]['image']; ?>" />
-<meta property="og:type" content="video.other">
-
-<script>
-  setTimeout(function(){
-    window.location='https://www.mlabs.com.br/';
-  },100);
-</script>
+https://media.giphy.com/media/j3x5hjUoXIesM/giphy.gif
+https://media.giphy.com/media/14UizCfaLFxm0/giphy.gif
+https://media.giphy.com/media/zAnqsnt6xecSs/giphy.gif
+https://media.giphy.com/media/VOBNMdR06JjwY/giphy.gif
+https://media.giphy.com/media/DGkRR2874Yb5u/giphy.gif
+https://media.giphy.com/media/rnkj4jBA6Yfcs/giphy.gif
+https://media.giphy.com/media/aKaDwkyzdIQvu/giphy.gif
+https://media.giphy.com/media/3o6UB5RrlQuMfZp82Y/giphy.gif
+https://media.giphy.com/media/3ohuPkgbcIaG2OoDIs/giphy.gif
+https://media.giphy.com/media/O9E8GuvetC1Ta/giphy.gif
+https://media.giphy.com/media/jv0TnQE80z8wU/giphy.gif
