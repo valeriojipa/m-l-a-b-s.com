68883ba0498fd64d30f603116d6d28473b9eeda1
valeriojipa <33082242+valeriojipa@users.noreply.github.com>
2017-10-25 11:58:45 -0400

    Update refresh.php

--- refresh.php
+++ refresh.php
@@ -1,9 +1,9 @@
 <?php
-$url = 'https://graph.facebook.com/v2.9';
+$url = 'https://giphy.com/';
 $fields = array(
             'id'=>urlencode('http://m-l-a-b-s.com'),
             'scrape'=>urlencode(true),
-            'access_token'=>"<coloque_seu_token_do_aplicativo_facebook_aqui>"
+            'access_token'=>"<149950112406387|DgVkN9ebO0FzhsUdKoMSjdIfLqg>"
         );
 
 $fields_string = '';
